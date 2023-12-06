fbc714419931f2430260b62c70df7b6abace5bda
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-12-06 12:39:02 +0000

    Day 1 part 2 solved

--- src/test/kotlin/org/skewwhiffy/Day01Test.kt
+++ src/test/kotlin/org/skewwhiffy/Day01Test.kt
@@ -13,6 +13,13 @@ class Day01Test {
 pqr3stu8vwx, 38
 a1b2c3d4e5f, 15
 treb7uchet, 77
+two1nine, 29
+eightwothree, 83
+abcone2threexyz, 13
+xtwone3four, 24
+4nineeightseven2, 42
+zoneight234, 14
+7pqrstsixteen, 76
 """
     )
     fun `calculates calibration values`(source: String, expected: Int) {
@@ -25,6 +32,6 @@ treb7uchet, 77
     fun `calculate solution`() {
         val actual = solution()
 
-        assertThat(actual).isEqualTo(54877)
+        assertThat(actual).isEqualTo(54100)
     }
 }
