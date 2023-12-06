dcdeefd7010107a439f78780326a734911b2fabe
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-12-06 12:39:24 +0000

    Day 1 part 2 solved (#2)

--- src/main/kotlin/org/skewwhiffy/Day01.kt
+++ src/main/kotlin/org/skewwhiffy/Day01.kt
@@ -33,6 +33,24 @@ treb7uchet
 In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
 Consider your entire calibration document. What is the sum of all of the calibration values?
+
+PART TWO
+
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters:
+one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
  */
 
 fun solution(): Int {
@@ -47,7 +65,18 @@ fun solution(): Int {
 }
 
 fun calculateCalibrationValue(source: String): Int {
-    val integers = source.filter { it.isDigit() }.map { it.digitToInt() }
+    val integers = source
+        .replace("one", "one1one")
+        .replace("two", "two2two")
+        .replace("three", "three3three")
+        .replace("four", "four4four")
+        .replace("five", "five5five")
+        .replace("six", "six6six")
+        .replace("seven", "seven7seven")
+        .replace("eight", "eight8eight")
+        .replace("nine", "nine9nine")
+        .filter { it.isDigit() }
+        .map { it.digitToInt() }
     val firstInteger = integers.first()
     val lastInteger = integers.last()
     return 10 * firstInteger + lastInteger
